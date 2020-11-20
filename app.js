3e933d00fef68a4e5dc12311b4549da6bdec6c07
Yomi <modamorioluwayomi@gmail.com>
2020-11-20 11:54:10 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+//Select element and variable
+
+const cartBtn = document.querySelector(".cart-btn")
+const closeCartBtn = document.querySelector(".close-btn")
+const clearCartBtn = document.querySelector(".clear-cart")
+const cartDom = document.querySelector(".cart")
+const cartOverlay = document.querySelector(".cart-overly")
+const cartItems = document.querySelector(".cart-items")
+const cartTotal = document.querySelector(".cart-total")
+const cartContent = document.querySelector(".cart-content")
+const productDom = document.querySelector(".product-center")
+
+
+//Cart Item
+let cart = []
+
+//getting the products 
+class Products{
+
+}
+
+//display productttttttttttttttt
+
+class UI{
+
+}
+
+class Storage{
+
+}
+
+document.addEventListener("DomcontentLoaded", ()=>{
+   const ui = new UI()
+   const product = new Products()
+})
