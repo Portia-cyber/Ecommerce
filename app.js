39ab7142b17438c039e0bb95c883ad126ef7c844
Yomi <modamorioluwayomi@gmail.com>
2020-11-24 04:06:02 +0100

    Update.js

--- app.js
+++ app.js
@@ -11,25 +11,67 @@ const cartContent = document.querySelector(".cart-content")
 const productDom = document.querySelector(".product-center")
 
 
+
+
 //Cart Item
 let cart = []
 
 //getting the products 
 class Products{
+  async getProducts(){
+     try {
+        let result = await fetch("products.json")
+        let data = await result.json()
+        let products = data.items
+        products = products.map (item =>{
+           const {title, price}= item.fields
+           const {id} = item.sys
+           const image = item.fields.image.fields.file.url
+           return{ title, price, id,image}
+        })
+        return products
+     } catch (error) {
+      console.log(error)  
+     }
+      
+   }
 
 }
-
 //display productttttttttttttttt
-
 class UI{
-
+displayProduct(products){
+   let result = '';
+   products.forEach(product => {
+      result +=`
+      <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="fas fa-shopping-cart">Add to Bag</i>
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$${product.price}</h4>
+        </article>
+      
+      
+      `
+   })
+   productDom.innerHTML = result;
+}
 }
 
 class Storage{
 
 }
 
-document.addEventListener("DomcontentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
    const ui = new UI()
    const product = new Products()
+   product.getProducts().then(products => ui.displayProduct(products))
 })
+
